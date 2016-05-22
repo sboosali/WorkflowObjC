4a8b9088751f7a59c0d018eda1c15687977076d3
Spiros Boosalis <samboosalis@gmail.com>
2016-05-22 14:16:55 +0300

    Initial Commit

---
+++ WorkflowObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WorkflowObjC
+//
+//  Created by Michael Boosalis on 5/22/16.
+//  Copyright © 2016 Michael Boosalis, Lord Of The Game. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
